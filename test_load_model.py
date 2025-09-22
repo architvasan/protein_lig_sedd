70831a8f74ee7145975cda24b97165ef0ac7bf78
architvasan <avasan@anl.gov>
2025-09-22 11:37:23 -0500

    reorganized readmes and scripts

--- test_load_model.py
+++
@@ -1,5 +0,0 @@
-from protlig_dd.training.run_train_protlig_special import Config
-from protlig_dd.model.transformers_prot_lig import ProteinLigandSharedDiffusion
-config = Config(yamlfile = 'configs/config_ddp.yaml')
-model = ProteinLigandSharedDiffusion(config).to('cuda')
-print(model)
